06ff7fd64fc23232d5df92504ea5d64bfe3aa1e4
Tamas Nagy <tamas@tamasnagy.com>
2017-01-23 18:39:50 -0800

    add moar tests

--- test/test_cluster.py
+++ test/test_cluster.py
@@ -11,3 +11,27 @@ def test_similarity():
 
     # update this assertion
     assert cluster.compute_similarity(activesite_a, activesite_b) == 0.0
+
+def test_partition_clustering():
+    # tractable subset
+    pdb_ids = [276, 4629, 10701]
+
+    active_sites = []
+    for id in pdb_ids:
+        filepath = os.path.join("data", "%i.pdb"%id)
+        active_sites.append(io.read_active_site(filepath))
+
+    # update this assertion
+    assert cluster.cluster_by_partitioning(active_sites) == []
+
+def test_hierarchical_clustering():
+    # tractable subset
+    pdb_ids = [276, 4629, 10701]
+
+    active_sites = []
+    for id in pdb_ids:
+        filepath = os.path.join("data", "%i.pdb"%id)
+        active_sites.append(io.read_active_site(filepath))
+
+    # update this assertion
+    assert cluster.cluster_hierarchically(active_sites) == []
