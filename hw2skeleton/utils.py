376deba666f11b429567e1f236fa72d29df186c0
Tamas Nagy <tamas@tamasnagy.com>
2017-01-25 13:34:05 -0800

    use new string formatting to get around missing conversion

--- hw2skeleton/utils.py
+++ hw2skeleton/utils.py
@@ -25,7 +25,7 @@ class Residue:
 
     # Overload the __repr__ operator to make printing simpler.
     def __repr__(self):
-        return self.type + " " + self.number
+        return "{0} {1}".format(self.type, self.number)
 
 class ActiveSite:
     """
